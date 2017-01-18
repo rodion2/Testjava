bb21804b563c232164a69ebf80c8883f572dd6e8
rodya <rodya@rodya-HP-Pavilion-g7-Notebook-PC>
2017-01-19 02:47:48 +0300

    unions and inrceptions added

--- src/Test2/Main.java
+++ src/Test2/Main.java
@@ -5,9 +5,53 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		Set mySet = new HashSet();
+		HashSet<Integer> mySet = new HashSet<>();
+		HashSet <Integer> mySet2 = new HashSet<>();
+		
+		Scanner in = new Scanner(System.in);
+		int x=in.nextInt();
+		addingMethod(x,mySet);
+		int y=in.nextInt();
+		addingMethod(y,mySet2);
+		
+		System.out.println(union(mySet,mySet2));
+		
+		System.out.println(interception(mySet,mySet2));
 		
 
+	}
+	
+	public static void addingMethod(int x,Set<Integer> a){
+			while(x > 0){
+						a.add(x%10);
+						x/=10;
+						} 
+	}
+	
+	public static Set<Integer> union(Set<Integer> a, Set<Integer> b)
+	{
+		Set<Integer> union = new HashSet<>();
+		union.addAll(a);
+		union.addAll(b);
+		return (union);
+	}
+	
+	public static HashSet<Integer> interception(HashSet<Integer> a, HashSet<Integer> b)
+	{
+		HashSet<Integer> interceptor = new HashSet<>();
+		
+		Iterator <Integer>iter = a.iterator();
+		
+		while (iter.hasNext())
+		{
+			Integer temp =iter.next(); 
+			if(b.contains(temp))
+			{
+				interceptor.add(temp);
+			}
+		}
+		
+		return (interceptor);
 	}
 
 }
